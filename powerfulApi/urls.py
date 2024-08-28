8ab9961fb40241a2e5c2219a64cc3f527dba061e
akanshachabra <akanshachhabra42@gmail.com>
2024-08-28 17:36:49 +0530

    Add existing project files to Git

---
+++ powerfulApi/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+from .views import ItemViewSet
+
+router = DefaultRouter()
+router.register(r'items', ItemViewSet)
+
+urlpatterns = router.urls
---
+++ powerfulApi/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Item
+from .serializers import ItemSerializer
+from rest_framework.permissions import IsAuthenticated
+
+
+# Create your views here.
+class ItemViewSet(viewsets.ModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+    permission_classes = [IsAuthenticated]
